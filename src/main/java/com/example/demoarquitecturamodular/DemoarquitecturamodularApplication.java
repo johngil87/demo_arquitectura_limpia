3f2ae865a92374e7d3446d2fb46874e43ccbecd7
johngil87 <johngil1487@gmail.com>
2021-07-14 19:50:18 -0500

    se crea la estructura de los paquetes

---
+++ src/main/java/com/example/demoarquitecturamodular/DemoarquitecturamodularApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoarquitecturamodular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoarquitecturamodularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoarquitecturamodularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoarquitecturamodular/DemoarquitecturamodularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoarquitecturamodular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoarquitecturamodularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
